09dcedbaa52dcd6faa4aba969603856d9690bde8
haleyprindle <haley@prindle.net>
2025-12-16 00:14:42 +0000

    done!

--- src/main/java/com/BrickBreak/Ball.java
+++ src/main/java/com/BrickBreak/Ball.java
@@ -32,57 +32,91 @@ public class Ball {
 	}
 	//methods:
 
+
+
+	//precondition: None
+	//postcondition: returns the ball's y value
 	public int getYpos()
 	{
 		return yPos;
 	}
 
+
+
+	//precondition: None
+ 	//postcondition: returns the ball's x value
 	public int getXpos()
 	{
 		return xPos;
 	}
 
+	//precondition: x is a non-null number
+	//postcondition: sets the x value of the ball equal to the inputed x
 	public void setX(int xPos)
 	{
 		this.xPos = xPos;
 	}
 
+
+
+	//precondition: y is a non-null number
+  	//postcondition: sets the y value of the ball equal to the inputed y
 	public void setY(int yPos)
 	{
 		this.yPos = yPos;
 	}
 
+
+	//precondition: none
+  	//postcondition: changes the x direction of the ball
 	public void reverseX()
 	{
 		xVelocity *=-1;
 	}
 
+
+	//precondition: None
+ 	//postcondition: changes the y direction of the ball
 	public void reverseY()
 	{
 		yVelocity *=-1;
 	}
 
+
+
+	//precondition: xVelocity is a non-null number
+	//postcondition: sets the xVelocity of the ball equal to the inputed xVelocity
 	public void setXVelocity(int xVelocity)
 	{
 		this.xVelocity = xVelocity;
 	}
 
+
+	//precondition: yVelocity is a non-null number
+	//postcondition: sets the yVelocity of the ball equal to the inputed yVelocity
 	public void setYVelocity(int yVelocity)
 	{
 		this.yVelocity = yVelocity;
 	}
 	
+	//precondition: none
+	//postcondition: returns the ball's size
 	public int getSize()
 	{
 		return size;
 	}
 
+	//precondition: None
+ 	//postcondition: moves the ball by xVelocity and yVelocity
 	public void move ()
 	{
 		xPos+=xVelocity;
 		yPos+=yVelocity;
 	}
 
+
+	//precondition: g is a non-null value
+	//postcondition: makes the ball on the screen
 	public void draw (Graphics g)
 	{
 		g.setColor(Color.BLUE); // call first to set the desired color
--- src/main/java/com/BrickBreak/Paddle.java
+++ src/main/java/com/BrickBreak/Paddle.java
@@ -35,31 +35,44 @@ public class Paddle {
 	}
 	//methods:
 
+
+	//precondition: none
+	//postcondition: returns the paddles' y
 	public int getY()
 	{
 		return y;
 	}
 
+
+
+	//precondition: none
+	//postcondition: returns the paddles' x
 	public int getX()
 	{
 		return x;
 	}
-
+	//precondition: none
+	//postcondition: returns the paddles' width
 	public int getWidth()
 	{
 		return width;
 	}
-
+	//precondition: none
+	//postcondition: returns the paddles' height
 	public int getHeight()
 	{
 		return height;
 	}
-
+	//precondition: none
+	//postcondition: returns the paddles' xVelocity
 	public int getVelocity()
 	{
 		return xVelocity;
 	}
 
+
+	//precondition: addVelocity is a non-null number
+ 	//postcondition: adds to the xVelocity of the paddle by addVelocityy to make the speed greater
 	public void addVelocity(int addVelocity)
 	{
 		if(!(addVelocity>0 && this.xVelocity>10) || !(addVelocity<0 && this.xVelocity<-10)
@@ -67,21 +80,27 @@ public class Paddle {
 			this.xVelocity+= addVelocity;
 		}
 	}	
-	
+	//precondition: x is a non-null number
+	//postcondition: sets the x value of the paddle equal to the inputed x
 	public void setX (int x)
 	{
 		this.x = x;
 	}
-
+	//precondition: xVelocity is a non-null number
+	//postcondition: sets the xVelocity value of the ball equal to the inputed xVelocity
 	public void setVelocity (int xVelocity)
 	{
 		this.xVelocity = xVelocity;
 	}
-
+	//precondition: None
+ 	//postcondition: moves the paddle by xVelocity
 	public void move() {
 	    x+=xVelocity;	 
 	 }
 
+
+	//precondition: g is a non-null value
+	//postcondition: draws the paddle on the screen
 	public void draw (Graphics g)
 	{
 		g.setColor(Color.BLUE); // call first to set the desired color
